90fb1552667cc7a2f831010b3b2849dadd3eab2f
Alireza Khorami <khoramism@gmail.com>
2021-08-27 14:01:55 +0430

    added the test func for GQL

--- precious/blog/tests.py
+++ precious/blog/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+# GRAPHQL tests
+import json
+import pytest
+from graphene_django.utils.testing import graphql_query
+
+@pytest.fixture
+def client_query(client):
+    def func(*args, **kwargs):
+        return graphql_query(*args, **kwargs, client=client)
+
+    return func
+
+# Test you query using the client_query fixture
+def test_some_query(client_query):
+    response = client_query(
+        '''
+        query {
+            post {
+                id
+                title
+            }
+        }
+        ''',
+        op_name='Post'
+    )
+    content = json.loads(response.content)
+    assert 'errors' not in content
