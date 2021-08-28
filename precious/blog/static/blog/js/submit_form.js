841646eec61d006e1112ce54428e40148830aa62
Amirali <liamirali.lotfi@gmail.com>
2021-08-28 21:59:18 +0430

    Commit after .gitignore fix for static folder

---
+++ precious/blog/static/blog/js/submit_form.js
@@ -0,0 +1,31 @@
+document.querySelector(".form-container .submit-container button").addEventListener("click", function(event){
+	event.preventDefault();
+	let temp;
+	document.getElementById("completed-search-form__word").innerHTML = "";
+	document.getElementById("completed-search-form__lang").innerHTML = "";
+	document.getElementById("completed-search-form__category").innerHTML = "";
+	document.getElementById("completed-search-form__stars").innerHTML = "";
+	document.getElementById("completed-search-form__submit").innerHTML = "";
+
+
+	temp = document.querySelector(".form-container #word-search").value;
+	document.getElementById("completed-search-form__word").append(temp);
+
+	temp = document.querySelectorAll(".form-container #dropdown__lang .dropdown-options.selected");
+	for (let i = 0; i < temp.length; i++) {
+		document.getElementById("completed-search-form__lang").append(temp[i].textContent + ",");
+	}
+
+	temp = document.querySelectorAll(".form-container #dropdown__category .dropdown-options.selected");
+	for (let i = 0; i < temp.length; i++) {
+		document.getElementById("completed-search-form__category").append(temp[i].textContent + ",");
+	}
+
+	temp = document.querySelector(".form-container #range-slider__min").value;
+	document.getElementById("completed-search-form__stars").append(temp + ",");
+
+	temp = document.querySelector(".form-container #range-slider__max").value;
+	document.getElementById("completed-search-form__stars").append(temp + ",");
+
+	document.getElementById("completed-search-form__submit").innerHTML = "true";
+});
