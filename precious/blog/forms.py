02f46e43434eaed092608cf0953422402de1388e
Alireza Khorami <khoramism@gmail.com>
2021-09-04 10:25:08 +0430

    Fixed the form id issue."

--- precious/blog/forms.py
+++ precious/blog/forms.py
@@ -98,7 +98,7 @@ class SearchForm(forms.Form):
     search.widget = forms.TextInput(attrs={'placeholder':"تایپ کنید: ",'name': 'email'})
 
 class TableSearchForm(forms.Form):
-    cats = forms.MultipleChoiceField(label="", choices=CATS, widget=forms.CheckboxSelectMultiple(attrs = {'class':"dropdown-options-container en" ,'id':"dropdown__lang"}))
-    langtags = forms.MultipleChoiceField(label="", choices=LANGTAGS, widget=forms.CheckboxSelectMultiple(), )
+    cats = forms.MultipleChoiceField(label="", choices=CATS, widget=forms.CheckboxSelectMultiple(attrs = {'class':"dropdown-options-container en" ,'id':"dropdown__cats"}))
+    langtags = forms.MultipleChoiceField(label="", choices=LANGTAGS, widget=forms.CheckboxSelectMultiple(attrs = {'class':"dropdown-options-container en" ,'id':"dropdown__lang"}), )
     stars = forms.IntegerField(label="ستاره ها")
     
